5b7deea7ff65d3259113023d67f5511903f054b6
leensungzero <brianwoo119@naver.com>
2019-08-01 01:17:10 +0900

    source code update

--- run.py
+++ run.py
@@ -1,32 +1,26 @@
 import tensorflow as tf
 import numpy as np
+import random
 
 
-fashion = np.loadtxt('./fashionmnist/fashion-mnist_test.csv', delimiter=',', dtype=np.float32, skiprows=1)
+fashion_label = ["T-shirt/top", "Trouser", "Pullover", "Dress",
+                 "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+fashion = np.loadtxt('./dataset/fashion-mnist_test.csv', delimiter=',', dtype=np.float32, skiprows=1)
 fashion /= 255.0
 x_data = fashion[:, 1:]
 y_data = fashion[:, [0]] * 255
 
-print("x_data:", x_data.shape)
-print("y_data:", y_data.shape)
-
 training_x_data = x_data[0:int(x_data.shape[0] * 0.7), :]
 training_y_data = y_data[0:int(y_data.shape[0] * 0.7), :]
-
-print("training_x_data:", training_x_data.shape)
-print("training_y_data:", training_y_data.shape)
-print(training_y_data)
 
 test_x_data = x_data[int(x_data.shape[0] * 0.7):, :]
 test_y_data = y_data[int(y_data.shape[0] * 0.7):, :]
 
-print("test_x_data:", test_x_data.shape)
-print("test_y_data:", test_y_data.shape)
 
 nb_classes = 10
 
 X = tf.placeholder(tf.float32, [None, 784])
-# Y = tf.placeholder(tf.float32, [None, 1])
 Y = tf.placeholder(tf.int32, [None, 1])
 
 
@@ -37,8 +31,6 @@ Y_one_hot = tf.reshape(Y_one_hot, [-1, nb_classes])
 
 W = tf.Variable(tf.random_normal([784, nb_classes], 3), name='weight')
 b = tf.Variable(tf.random_normal([nb_classes], 3), name='bias')
-# W = tf.Variable(tf.zeros([784, nb_classes]), name='weight')
-# b = tf.Variable(tf.zeros([nb_classes]), name='bias')
 
 
 # hypothesis = tf.nn.softmax(tf.matmul(X, W) + b)
@@ -58,7 +50,7 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
 
-    for step in range(13001):
+    for step in range(10001):
         loss, acc, _ = sess.run([cost, accuracy, optimizer], feed_dict={X: training_x_data, Y: training_y_data})
 
         if step % 50 == 0:
@@ -67,3 +59,7 @@ with tf.Session() as sess:
             ))
 
     print("Accuracy: {:2%}".format(accuracy.eval(session=sess, feed_dict={X: test_x_data, Y: test_y_data})))
+
+    r = random.randint(0, test_y_data.shape[0] - 1)
+    print("Label:", fashion_label[int(sess.run(tf.argmax(Y_one_hot[r:r+1], 1), feed_dict={Y: test_y_data})[0])])
+    print("Prediction:", fashion_label[int(sess.run(tf.argmax(hypothesis, 1), feed_dict={X: test_x_data[r:r+1]}))])
